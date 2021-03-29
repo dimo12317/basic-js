25813e34f3adec739070ef92385c76e52ee56c6b
dimo12317 <79600828+dimo12317@users.noreply.github.com>
2021-03-30 01:52:41 +0300

    Update vigenere-cipher.js

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,92 @@
 const CustomError = require("../extensions/custom-error");
-
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+  constructor(a){
+    if(a === false){
+        this.typeMachine = false;
+    }else{
+        this.typeMachine = true;
+    }
+}
+  encrypt(strparam, strkey) {
+    if(strparam === undefined || strkey === undefined)
+    return  new Error(THROWN);
+    if(typeof(strparam) != "string" || typeof(strkey) != "string")
+    return new Error(THROWN);
+    let y = 0;
+    let str = [];
+    strparam = strparam.split("");
+    strkey = strkey.split("");
+    for(let i = 0; i < strparam.length; i++){
+      strparam[i] = strparam[i].toLowerCase();
+    }
+    for(let i = 0; i < strkey.length; i++){
+      strkey[i] = strkey[i].toLowerCase();
+    }
+    for(let i = 0; i < strparam.length; i++){
+            if(strparam[i].charCodeAt() < 97 || strparam[i].charCodeAt() > 122){
+                str[i] = strparam[i];
+                --y;
+            }
+            else if(strparam[i].charCodeAt() + strkey[y].charCodeAt() - 97 > 122){
+              str[i] = String.fromCharCode(strparam[i].charCodeAt() + strkey[y].charCodeAt()-123)
+            }else{
+              str[i] = String.fromCharCode(strparam[i].charCodeAt() + strkey[y].charCodeAt()-97)
+            }
+            if(str[i] === '`')
+            str[i] = "z";
+        ++y;
+        if(y === strkey.length)
+        y = 0;
+    }
+    for(let i = 0; i < str.length; i++){
+      str[i] = str[i].toUpperCase();
+    }
+    if(this.typeMachine === false)
+    str = str.reverse();
+    str = str.join("");
+    return str;
+}
+  decrypt(strparam, strkey) {
+    if(strparam === undefined || strkey === undefined)
+    return new Error(THROWN);
+    if(typeof(strparam) != "string" || typeof(strkey) != "string")
+    return new Error(THROWN);
+    let y = 0;
+    let str = [];
+    strparam = strparam.split("");
+    strkey = strkey.split("");
+    for(let i = 0; i < strparam.length; i++){
+        strparam[i] = strparam[i].toLowerCase();
+    }
+    for(let i = 0; i < strkey.length; i++){
+        strkey[i] = strkey[i].toLowerCase();
+    }
+    for(let i = 0; i < strparam.length; i++){
+      if(strparam[i].charCodeAt() < 97 || strparam[i].charCodeAt() > 122){
+          str[i] = strparam[i];
+          --y;
+      }
+      else if(strparam[i].charCodeAt() - strkey[y].charCodeAt() + 97 >= 96){
+        str[i] = String.fromCharCode(strparam[i].charCodeAt() - strkey[y].charCodeAt() + 97)
+      }else{
+        str[i] = String.fromCharCode(strparam[i].charCodeAt() - strkey[y].charCodeAt() + 123)
+      }
+      if(str[i] === '`')
+      str[i] = "z";
+        ++y;
+        if(y === strkey.length)
+        y = 0;
+    }
+    if(this.typeMachine === false)
+    strparam.reverse()
+    for(let i = 0; i < str.length; i++){
+      str[i] = str[i].toUpperCase();
+    }
+    if(this.typeMachine === false)
+        str.reverse();
+    str = str.join("");
+    return str;
+}
 }
 
 module.exports = VigenereCipheringMachine;
