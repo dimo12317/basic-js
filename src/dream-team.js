54ce0351bceea6d811c38ff7098f3991091dea4c
dimo12317 <79600828+dimo12317@users.noreply.github.com>
2021-03-30 01:46:09 +0300

    Update dream-team.js

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,43 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(params) {
+if(typeof(params) === "object"){
+    if(params === null || params.constructor != Array){
+        return false
+    }
+}else{
+    return false;
+}
+for(let i = 0; i < params.length; i++){
+    if(typeof(params[i]) != "string"){
+      params.splice(i,1);
+      i--;
+    }
+  }
+let nameTeam = [];
+for(let i = 0; i < params.length; i++){
+    if(params[i][0] === " "){
+        params[i] = params[i].slice(1,params[i].length)
+        i--;
+    }
+}
+for(let i = 0; i < params.length; i++){
+    if(typeof(params[i]) === "string")
+    nameTeam.push(params[i][0].toLowerCase());
+}
+console.log(nameTeam)
+for(let i = 0; i < nameTeam.length; i++){
+    for(let y = i; y < nameTeam.length; y++){
+        if(nameTeam[i] > nameTeam[y]){
+            let a = nameTeam[y];
+            nameTeam[y] = nameTeam[i];
+            nameTeam[i] = a;
+        }
+    }
+}
+console.log(nameTeam)
+for(let i = 0; i < nameTeam.length; i++){
+    nameTeam[i] = nameTeam[i].toUpperCase()
+}
+return nameTeam.join("")
 };
